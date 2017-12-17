3e32d0e070c53952913a1283c34ce08979c793b8
paperrin <paperrin@student.42.fr>
2017-12-17 14:28:39 +0100

    Fixed .gitignore because of src/obj which was taken by rule obj/

---
+++ src/obj/obj_sphere.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   obj_sphere.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paperrin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/16 16:22:11 by paperrin          #+#    #+#             */
+/*   Updated: 2017/12/16 19:20:06 by paperrin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rt.h"
+
+t_obj		obj_sphere(t_real3 pos, t_real radius, t_mat_id material)
+{
+	t_obj		obj;
+
+	obj.type = type_sphere;
+	obj.material = material;
+	obj.as.sphere.pos = pos;
+	obj.as.sphere.normal = vec3r(0, 0, -1);
+	obj.as.sphere.radius = radius;
+	return (obj);
+}
